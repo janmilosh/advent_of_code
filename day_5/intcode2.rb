daecd43a4466e0f5d50df6c36135a94032155165
Jan Milosh <jmilosh@covermymeds.com>
2019-12-06 10:57:30 -0500

    Day 5 problem 2 complete

--- day_5/intcode2.rb
+++ day_5/intcode2.rb
@@ -19,7 +19,7 @@ class Intcode2
   end
 
   def input
-    print "\nInput: "
+    print "Input: "
     gets.chomp[0].to_i
   end
 
@@ -28,16 +28,7 @@ class Intcode2
   end
 
   def process_opcode_1(instruction, p1, p2, p3)
-    if p1 == 0
-      p1_value = data_array[instruction[1]]
-    else
-      p1_value = data_array[pointer + 1]
-    end
-    if p2 == 0
-      p2_value = data_array[instruction[2]]
-    else
-      p2_value = data_array[pointer + 2]
-    end
+    p1_value, p2_value = p1_p2_values(instruction, p1, p2)
     value = p1_value + p2_value
     if p3 == 0
       data_array[instruction[3]] = value
@@ -48,16 +39,7 @@ class Intcode2
   end
 
   def process_opcode_2(instruction, p1, p2, p3)
-    if p1 == 0
-      p1_value = data_array[instruction[1]]
-    else
-      p1_value = data_array[pointer + 1]
-    end
-    if p2 == 0
-      p2_value = data_array[instruction[2]]
-    else
-      p2_value = data_array[pointer + 2]
-    end
+    p1_value, p2_value = p1_p2_values(instruction, p1, p2)
     value = p1_value * p2_value
     if p3 == 0
       data_array[instruction[3]] = value
@@ -81,6 +63,68 @@ class Intcode2
     @pointer += instruction.length
   end
 
+  def process_opcode_5(instruction, p1, p2)
+    p1_value, p2_value = p1_p2_values(instruction, p1, p2)
+    if p1_value != 0
+      @pointer = p2_value
+    else
+      @pointer += instruction.length
+    end
+  end
+
+  def process_opcode_6(instruction, p1, p2)
+    p1_value, p2_value = p1_p2_values(instruction, p1, p2)
+    if p1_value == 0
+      @pointer = p2_value
+    else
+      @pointer += instruction.length
+    end
+  end
+
+  def process_opcode_7(instruction, p1, p2, p3)
+    p1_value, p2_value = p1_p2_values(instruction, p1, p2)
+    if p1_value < p2_value
+      value = 1
+    else
+      value = 0
+    end
+    if p3 == 0
+      data_array[instruction[3]] = value
+    else
+      data_array[pointer + 3] = value
+    end
+    @pointer += instruction.length
+  end
+
+  def process_opcode_8(instruction, p1, p2, p3)
+    p1_value, p2_value = p1_p2_values(instruction, p1, p2)
+    if p1_value == p2_value
+      value = 1
+    else
+      value = 0
+    end
+    if p3 == 0
+      data_array[instruction[3]] = value
+    else
+      data_array[pointer + 3] = value
+    end
+    @pointer += instruction.length
+  end
+
+  def p1_p2_values(instruction, p1, p2)
+    if p1 == 0
+      p1_value = data_array[instruction[1]]
+    else
+      p1_value = data_array[pointer + 1]
+    end
+    if p2 == 0
+      p2_value = data_array[instruction[2]]
+    else
+      p2_value = data_array[pointer + 2]
+    end
+    return p1_value, p2_value
+  end
+
   def process(instruction, code_hash)
     opcode = code_hash[:opcode]
     p1 = code_hash[:p1]
@@ -91,6 +135,10 @@ class Intcode2
     process_opcode_2(instruction, p1, p2, p3) if opcode == 2
     process_opcode_3(instruction) if opcode == 3
     process_opcode_4(instruction, p1) if opcode == 4
+    process_opcode_5(instruction, p1, p2) if opcode == 5
+    process_opcode_6(instruction, p1, p2) if opcode == 6
+    process_opcode_7(instruction, p1, p2, p3) if opcode == 7
+    process_opcode_8(instruction, p1, p2, p3) if opcode == 8
   end
 
   def step
@@ -110,6 +158,8 @@ class Intcode2
   def instruction_length(opcode)
     return 2 if opcode == 3
     return 2 if opcode == 4
+    return 3 if opcode == 5
+    return 3 if opcode == 6
     return 4
   end
 
